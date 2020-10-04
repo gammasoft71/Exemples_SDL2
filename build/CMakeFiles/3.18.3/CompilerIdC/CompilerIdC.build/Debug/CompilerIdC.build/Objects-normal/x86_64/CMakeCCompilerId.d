9756f331b816a7a84644a26635440cdb5acb6d89
Gammasoft71 <gammasoft71@gmail.com>
2020-10-04 14:27:20 +0200

    remove build

--- build/CMakeFiles/3.18.3/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/x86_64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/yves/Projects/Examples/Examples_SDL2/build/CMakeFiles/3.18.3/CompilerIdC/CMakeCCompilerId.c
--- build/SDL2Examples.build/Debug/ZERO_CHECK.build/Script-C7CE7565CD8C46E3991F4616.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/yves/Projects/Examples/Examples_SDL2/build -f /Users/yves/Projects/Examples/Examples_SDL2/build/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION OBJDIR=$(basename "$OBJECT_FILE_DIR_normal") all
